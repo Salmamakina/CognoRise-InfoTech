798e0fc1d30838fe6d3d0c538cf513985bcf909e
salma <salma.makinaa2557@gmail.com>
2024-06-25 06:36:09 +0100

    commit in the main

---
+++ NumberGuessingGame/src/Graphic.java
@@ -0,0 +1,47 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Graphic {
+    private JFrame frame;
+    private CardLayout cardLayout;
+    private JPanel mainPanel;
+    Random randomData = new Random();
+    private int randomNumber = randomData.nextInt(1, 100 + 1);
+    Scanner scanner = new Scanner(System.in);
+    private int userInput = scanner.nextInt();
+    private boolean guessedCorrectly = false;
+    private int attempts = 0;
+    private int maxAttempts = 10;
+    public Graphic(){
+        frame = new JFrame("Number Guessing Game");
+        cardLayout = new CardLayout();
+        mainPanel = new JPanel(cardLayout);
+
+
+        mainPanel.add(createNumberChoosingPanel(), "ChooseNumber");
+        mainPanel.add(createMessagePanel(), "Message");
+        frame.add(mainPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400, 300);
+        frame.setVisible((true));
+        startNewGame();
+
+    }
+    private JPanel createNumberChoosingPanel() {
+        JPanel panel = new JPanel();
+        panel.setLayout(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.insets = new Insets(10, 10, 10, 10);
+    }
+
+    private String createMessagePanel() {
+    }
+
+    private void startNewGame() {
+    }
+
+}
